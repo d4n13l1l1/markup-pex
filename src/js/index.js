7e553415c410cf28e5cbbcdda48458ea4b5acb99
Daniel Ilich <d4niel.iv@gmail.com>
2018-09-17 18:48:09 +0300

    Save uncommited changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,5 @@
 import '../css/main.scss';
 
-const el = document.querySelector('h1');
-
-el.textContent += 'World!';
-
-document.body.appendChild(el);
-
 if (module.hot) {
   module.hot.accept();
 }
